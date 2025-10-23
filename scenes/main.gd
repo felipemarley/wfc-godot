f982807538871aa7994a9581fe70d02ca0c08dcb
felipemarley <felipemarley34@gmail.com>
2025-10-22 22:40:21 -0300

    ajustes para aplicacao da engenharia inversa

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,10 +1,9 @@
 extends Node2D
+
 @onready var wfc_generator = $WFC2DGenerator
 
 func _ready():
-	print("Iniciando a geração do mapa WFC...")
+	print("Iniciando a geração com regras de Terrains...")
 	wfc_generator.start()
 	await wfc_generator.done
-	print("Geração do mapa concluída!")
-	#await RenderingServer.frame_post_draw
-	#get_viewport().get_texture().get_image().save_png("user://Screenshot.png")
+	print("Geração concluída!")
